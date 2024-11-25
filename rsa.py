d48ef7e38ff1e6c93a9f1cb8f40bb2eb96efbd7d
Voore Manuchandana <143800426+ManuChandana@users.noreply.github.com>
2024-11-25 23:58:50 +0530

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,29 +1,19 @@
-def gcd(a, b): 
-    while b: 
-        a, b = b, a % b
-    return a
-def extended_gcd(a, b):
-    if a == 0: 
-        return b, 0, 1
-    g, x1, y1 = extended_gcd(b % a, a)
-    return g, y1 - (b // a) * x1, x1
-def mod_inverse(e, phi):
-    g, x, _ = extended_gcd(e, phi)
-    return x % phi if g == 1 else None
-p, q = int(input("Enter p (prime): ")), int(input("Enter q (prime): "))
-n, phi = p * q, (p - 1) * (q - 1)
-e = next(i for i in range(2, phi) if gcd(i, phi) == 1)
-d = mod_inverse(e, phi)
-if d is None:
-    print("Invalid keys, modular inverse could not be found.")
-else:
-    msg = int(input("Enter a plaintext message (as an integer): "))
-    if msg < n:
-        c = pow(msg, e, n)
-        decrypted_msg = pow(c, d, n)
-        print("Public key: (e =", e, ", n =", n, ")")
-        print("Private key: (d =", d, ", n =", n, ")")
-        print("Encrypted message:", c)
-        print("Decrypted message:", decrypted_msg)
-    else:
-        print("Message must be less than n =", n)
+def gcd(a, h):
+    while True:
+        temp = a % h
+        if temp == 0:
+            return h
+        a, h = h, temp
+
+p = int(input("Enter p: "))
+q = int(input("Enter q: "))
+n, phi, e = p * q, (p - 1) * (q - 1), 2
+while e < phi:
+    if gcd(e, phi) == 1: break
+    e += 1
+d = (1 + (2 * phi)) // e
+msg = int(input("Enter message: "))
+c = pow(msg, e, n)
+m = pow(c, d, n)
+print("Encrypted:", c)
+print("Decrypted:", m)
